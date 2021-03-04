53a6090846909e98d2fa2c4526be4f40731c3295
Scott Vance <logic@newbaselogic.com>
2021-03-04 14:19:11 -0500

    - Added a script for compressing videso

---
+++ scripts/HdsCompressVideo.ps1
@@ -0,0 +1,19 @@
+#!/usr/local/bin/pwsh
+
+param(
+    [string]$src=$(throw '-src required'),
+    [string]$dest,
+    [int]$crf=28
+)
+
+if(!$dest){
+    $i=$src.LastIndexOf('.')
+    if($i -eq -1){
+        throw "-dest required"
+    }
+    $dest=$src.Substring(0,$i)+'(Compressed)'+$src.Substring($i)
+}
+
+Write-Host "$src -> $dest"
+
+ffmpeg -i $src -crf $crf $dest
