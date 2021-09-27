4e7f444d8a5b66c7ab5b145263b4f7b24b2d8d12
Scott Vance <logic@newbaselogic.com>
2021-09-27 07:51:22 -0400

    - Added Snippet helpers

---
+++ scripts/HdsCodeSnippets.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+code ~/Library/Application\ Support/Code/User/snippets/
---
+++ scripts/HdsSnippet.ps1
@@ -0,0 +1,41 @@
+#!/usr/local/bin/pwsh
+
+param(
+    [string]$source
+)
+
+$ErrorActionPreference="Stop"
+
+[string]$content=''
+[bool]$clipboard=$false
+
+if($source){
+    $content=Get-Content -Raw -Path $source
+    $clipboard=$false
+}else{
+    $content=Get-Clipboard -Raw
+    $clipboard=$true
+}
+
+$lines=$content.Split("`n");
+for($i=0;$i -lt $lines.Length;$i++){
+    $lines[$i]=$lines[$i].Trim("`r")
+}
+
+$obj=[ordered]@{
+    __SNIPPET__NAME__=[ordered]@{
+        scope="";
+        prefix="";
+        description="";
+        body=$lines;
+    }
+}
+
+$content=($obj | ConvertTo-Json).ToString()
+
+if($clipboard){
+    Set-Clipboard $content
+    Write-Host 'Snippent copied to clipboard'
+}else{
+    Write-Host $content
+}
