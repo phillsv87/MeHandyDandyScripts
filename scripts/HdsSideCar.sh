ea4ef5e835b81c9aa39b283aef3da7c50daafde5
Scott Vance <scott@iyio.io>
2022-06-16 15:52:28 -0400

    - Added script to open side car

---
+++ scripts/HdsSideCar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+osascript -e '
+tell application "System Preferences"
+    activate
+    reveal pane id "com.apple.preference.displays"
+    delay 1
+    tell application "System Events" to click first pop up button of first window of application process "System Preferences" of application "System Events"
+    delay 1
+    tell application "System Events" to click menu item "iPad (2)" of first menu of first pop up button of first window of application process "System Preferences" of application "System Events"
+    quit
+end tell';
